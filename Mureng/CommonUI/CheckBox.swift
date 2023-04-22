db36acd708d1c04b022f5d5f1ac407deaa316926
RoadtoS7 <kshvv975@gmail.com>
2023-04-22 17:39:49 +0900

    feat: CheckBox 컴포넌트 구현
    - 체크 토글 버튼에 해당한다.

---
+++ Mureng/CommonUI/CheckBox.swift
@@ -0,0 +1,20 @@
+//
+//  CheckBox.swift
+//  Mureng
+//
+//  Created by 김수현 on 2023/04/22.
+//
+
+import UIKit
+
+class CheckButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setImage(Images.checkboxOn24.image, for: .normal)
+        setImage(Images.checkboxOff24.image, for: .selected)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

da0103f328dd56d3982d1376b4b055db1675f7f1
RoadtoS7 <kshvv975@gmail.com>
2023-05-05 18:22:21 +0900

    feat: grey-1-disabled1 컬러 추가

--- Mureng/Source/Colors+Generated.swift
+++ Mureng/Source/Colors+Generated.swift
@@ -22,6 +22,7 @@ internal enum Colors {
   internal static let black = ColorAsset(name: "black")
   internal enum Grey {
     internal static let dark1 = ColorAsset(name: "dark1")
+    internal static let grey1Disabled1 = ColorAsset(name: "grey-1-disabled1")
     internal static let grey2Default1 = ColorAsset(name: "grey2-default1")
     internal static let greylight0 = ColorAsset(name: "greylight0")
     internal static let light2Placeholder = ColorAsset(name: "light2-placeholder")

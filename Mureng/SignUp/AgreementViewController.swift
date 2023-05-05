eabd216cd3ae4a63386b05d732a0c3ccff68179f
RoadtoS7 <kshvv975@gmail.com>
2023-05-05 17:43:45 +0900

    feat: ArrowCheckBoxView 파일 분리

--- Mureng/SignUp/AgreementViewController.swift
+++ Mureng/SignUp/AgreementViewController.swift
@@ -8,63 +8,6 @@
 import UIKit
 import SnapKit
 
-class ArrowCheckBoxView: UIView {
-    private var checkButton: CheckButton!
-    private var label: UILabel!
-    
-    convenience init(title: String) {
-        self.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = false
-        label.text = title
-        layoutIfNeeded()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initViews()
-        checkButton.addTouchAction { checkButton in
-            checkButton.isSelected.toggle()
-        }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func initViews() {
-        let stackView: UIStackView = .init()
-        stackView.axis = .horizontal
-        stackView.distribution = .fill
-        stackView.alignment = .center
-        addSubview(stackView)
-        stackView.snp.makeConstraints { make in
-            make.leading.trailing.top.bottom.equalToSuperview()
-        }
-        
-        let checkButton: CheckButton = .init()
-        checkButton.setContentHuggingPriority(.required, for: .horizontal)
-        checkButton.setContentHuggingPriority(.required, for: .vertical)
-        checkButton.snp.makeConstraints { make in
-            make.width.height.equalTo(24)
-        }
-        stackView.addArrangedSubview(checkButton)
-        self.checkButton = checkButton
-        
-        let label: UILabel = .init()
-        label.font = FontFamily.AppleSDGothicNeo.regular.font(size: 14)
-        stackView.addArrangedSubview(label)
-        self.label = label
-        
-        let arrowImageView: UIImageView = .init(image: Images.iconsChevronMore24.image)
-        arrowImageView.setContentHuggingPriority(.required, for: .horizontal)
-        arrowImageView.setContentHuggingPriority(.required, for: .vertical)
-        arrowImageView.snp.makeConstraints { make in
-            make.width.height.equalTo(24)
-        }
-        stackView.addArrangedSubview(arrowImageView)
-    }
-}
-
 class AgreementViewController: UIViewController {
     private enum Constant {
         static let title = "먼저 이용약관을 읽고\n동의해주세요."

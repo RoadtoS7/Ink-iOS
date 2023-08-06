4031ea498af9ef27845ed9398c18cf8846392389
RoadtoS7 <kshvv975@gmail.com>
2023-08-06 19:55:50 +0900

    feat: 회원가입할 때 필요한 유저 정보를 담는 SignUpUser 구조체 선언

--- Mureng/API/Member/MemberAuthAPI.swift
+++ Mureng/API/Member/MemberAuthAPI.swift
@@ -212,3 +212,25 @@ class MemberAuthAPI {
 struct NicknameDuplicatedDTO: Decodable {
     let duplicated: Bool
 }
+
+struct SignUpUser {
+    let identifier: String
+    let email: String
+    let image: String
+    let nickname: String? = nil
+    
+    func enoughForSignUp() -> Bool {
+        guard let nickname = nickname else {
+            return false
+        }
+        
+        guard identifier.isNotEmpty,
+              email.isNotEmpty,
+              nickname.isNotEmpty,
+              image.isNotEmpty else {
+            return false
+        }
+        
+        return true
+    }
+}

fc0040bde1a79c8d2bb9b26358cb75a0238bed8f
RoadtoS7 <kshvv975@gmail.com>
2023-09-03 18:09:09 +0900

    design: 오늘의 표현을 나타내는 뷰 구현

---
+++ Mureng/Home/ExpressionRowView.swift
@@ -0,0 +1,64 @@
+//
+//  ExpressionRowView.swift
+//  Mureng
+//
+//  Created by 김수현 on 2023/09/03.
+//
+
+import SwiftUI
+
+struct EnglishExpression {
+    let id: Int
+    let content: String
+    let koConent: String
+    let example: String
+    let koExample: String
+}
+
+struct ExpressionRowView: View {
+    let engExpression: EnglishExpression
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Rectangle()
+                .frame(minWidth: 3, maxWidth: 3, minHeight: 3, maxHeight: .infinity)
+                .background(Colors.Grey.grey2Default1.swiftUIColor)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                HStack(alignment: .firstTextBaseline, spacing: 12) {
+                    Text(engExpression.content)
+                    Text(engExpression.koConent)
+                }
+                .font(FontFamily.Pretendard
+                    .semiBold
+                    .swiftUIFont(size: 16)
+                )
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    Text(engExpression.example)
+                    Text(engExpression.koExample)
+                }
+                .font(FontFamily.Pretendard
+                    .regular
+                    .swiftUIFont(size: 14)
+                )
+                .foregroundColor(Colors.Grey.dark1.swiftUIColor)
+            }
+        }
+    }
+}
+
+struct ExpressionRowView_Previews: PreviewProvider {
+    private static var sampleExpression: EnglishExpression = .init(
+        id: 0,
+        content: "can’t wait to ~",
+        koConent: "얼른 ~하고 싶다",
+        example: "I can’t wait to go on this trip.",
+        koExample: "얼른 여행을 떠났으면 좋겠어."
+    )
+    
+    static var previews: some View {
+        ExpressionRowView(engExpression: sampleExpression)
+            .frame(maxWidth: .infinity, minHeight: 72, maxHeight: 72, alignment: .leading)
+    }
+}

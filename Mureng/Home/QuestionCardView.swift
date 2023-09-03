6c7e5bf99d4641371dcaadd5f6769c6647a73085
RoadtoS7 <kshvv975@gmail.com>
2023-09-03 15:59:10 +0900

    design: QuestionCardView 구현

--- Mureng/Home/QuestionCardView.swift
+++ Mureng/Home/QuestionCardView.swift
@@ -13,23 +13,41 @@ struct Question {
     let koreanContent: String
 }
 
-struct QuestionCard: View {
+struct QuestionCardView: View {
     let question: Question
+    let refreshAction: () -> Void
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 24) {
             Text(question.content)
+                .font(FontFamily.NanumMyeongjo.regular.swiftUIFont(size: 32)
+                    .weight(.heavy)
+                )
+                
             Text(question.koreanContent)
                 .font(FontFamily.Pretendard.regular.swiftUIFont(size: 16))
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    refreshAction()
+                }, label: {
+                    Images.iconsRefresh.swiftUIImage
+                })
+                .frame(width: 32, height: 32)
+            }
         }
+        .multilineTextAlignment(.center)
         .padding(.horizontal, 20)
         .padding(.vertical, 78)
         .background(Colors.white.swiftUIColor)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     private static let question: Question = .init(id: 0, content: "What do you want?", koreanContent: "어떤 습관을 만들고 싶나요?")
     static var previews: some View {
-        QuestionCard(question: question)
+        QuestionCardView(question: question, refreshAction: {})
     }
 }
--- Mureng/Source/Images+Generated.swift
+++ Mureng/Source/Images+Generated.swift
@@ -22,12 +22,12 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Images {
-  internal static let image = ImageAsset(name: "Image")
   internal static let inkIcon = ImageAsset(name: "InkIcon")
   internal static let checkboxOff24 = ImageAsset(name: "checkbox-off-24")
   internal static let checkboxOn24 = ImageAsset(name: "checkbox-on-24")
   internal static let chevronMore24 = ImageAsset(name: "chevron-more-24")
   internal static let iconsChevronMore24 = ImageAsset(name: "iconsChevronMore24")
+  internal static let iconsRefresh = ImageAsset(name: "icons_refresh")
   internal static let imgSplashLogo = ImageAsset(name: "img_splash_logo")
   internal static let kakaoLoginButton = ImageAsset(name: "kakaoLoginButton")
   internal static let logoDark112 = ImageAsset(name: "logoDark112")

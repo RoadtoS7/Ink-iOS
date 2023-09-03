2c2998fbfd35437fb46a23fe01b1dbc8983c05e2
RoadtoS7 <kshvv975@gmail.com>
2023-09-03 18:34:53 +0900

    design: HomeScreenView에서 TodayExpressionListView를 사용하도록 수정

--- Mureng/Home/HomeScreenView.swift
+++ Mureng/Home/HomeScreenView.swift
@@ -9,28 +9,64 @@ import SwiftUI
 
 struct HomeScreenView: View {
     let question: Question
+    @State var todayExpressions: [EnglishExpression]
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             HomeHeaderView()
+                .padding(.bottom, 28)
+            
             QuestionCardView(question: question, refreshAction: {})
+                .padding(.bottom, 24)
+                .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 4)
+            
             Button("글쓰기", action: {
                 
             })
             .buttonStyle(ButtonSoild48Style())
             .frame(height: 48)
-            Text("오늘의 표현")
-                .font(FontFamily.Pretendard.regular.swiftUIFont(size: 16))
+            .padding(.bottom, 28)
             
+            VStack(spacing: 20) {
+                Text("오늘의 표현")
+                .font(FontFamily.Pretendard.regular.swiftUIFont(size: 16))
+                .frame(maxWidth: .infinity, alignment: .leading)
                 
+                TodayExpressionListView(todayExpressions: todayExpressions)
+            }
+            .padding(.vertical, 20)
         }
         .padding(.horizontal, 24)
     }
 }
 
 struct HomeScreenView_Previews: PreviewProvider {
-    private static var question: Question = .init(id: 0, content: "What do you want?", koreanContent: "어떤 습관을 만들고 싶나요?")
+    private static var question: Question = .init(
+        id: 0,
+        content: "What do you want?",
+        koreanContent: "어떤 습관을 만들고 싶나요?"
+    )
+    
+    private static var todayExprssions: [EnglishExpression] = [
+        .init(
+            id: 0,
+            content: "can’t wait to ~",
+            koConent: "얼른 ~하고 싶다",
+            example: "I can’t wait to go on this trip.",
+            koExample: "얼른 여행을 떠났으면 좋겠어."
+        ),
+        .init(
+            id: 0,
+            content: "can’t wait to ~",
+            koConent: "얼른 ~하고 싶다",
+            example: "I can’t wait to go on this trip.",
+            koExample: "얼른 여행을 떠났으면 좋겠어."
+        ),
+    ]
+    
     static var previews: some View {
-        HomeScreenView(question: question)
+        HomeScreenView(question: question,
+                       todayExpressions: todayExprssions)
     }
 }
 

97394933c7556a240024db2608615c0187225c27
RoadtoS7 <kshvv975@gmail.com>
2024-06-23 12:43:22 +0900

    Refactor SceneDelegate, AppDelegate 대신 SwiftUI App을 사용하도록 수정

---
+++ Mureng/App/MurengApp.swift
@@ -0,0 +1,33 @@
+//
+//  MurengApp.swift
+//  Mureng
+//
+//  Created by 김수현 on 6/23/24.
+//
+
+import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+
+@main
+struct YourApp: App {
+    private let nativeAppKey: String = "81e6d349cfd84771e8e20876e041773c"
+    private let service: AuthenticationService = DefaultAuthService()
+    
+    
+    init() {
+        KakaoSDK.initSDK(appKey: nativeAppKey)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            EntryView(authenticationService: service)
+                .onOpenURL(perform: { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        AuthController.handleOpenUrl(url: url)
+                    }
+                })
+        }
+    }
+}
+

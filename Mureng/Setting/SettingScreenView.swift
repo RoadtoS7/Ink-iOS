4a3d8933d71192b2387c012f3e80f1bdd3611d1c
RoadtoS7 <kshvv975@gmail.com>
2024-07-07 13:29:21 +0900

    Feat 설정화면 디자인 구현 완료

---
+++ Mureng/Setting/SettingScreenView.swift
@@ -0,0 +1,85 @@
+//
+//  SettingScreenView.swift
+//  Mureng
+//
+//  Created by 김수현 on 7/7/24.
+//
+
+import SwiftUI
+
+struct SettingScreenView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                SettingItemView(title: "푸시 알림")
+                SettingItemView(title: "차단 사용자 관리")
+                divider
+                SettingItemView(title: "이용 약관")
+                SettingItemView(title: "개인정보 처리방침")
+                SettingItemView(title: "오픈소스 라이센스")
+                divider
+                SettingItemView(title: "로그아웃")
+                SettingItemView(title: "회원 탈퇴")
+            }
+            .font(.body)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+        }
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                HStack(spacing: 4, content: {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                        Images.leftTouch.swiftUIImage
+                    })
+                    
+                    Text("설정")
+                        .foregroundStyle(
+                            Colors
+                                .Neutral
+                                .Label
+                                .tertiary
+                                .swiftUIColor
+                        )
+                        .font(.title18R)
+                })
+            }
+        })
+    }
+    
+    var divider: some View {
+        Divider()
+            .foregroundStyle(
+                Colors 
+                    .Greyscale
+                    .greyscale200
+                    .swiftUIColor
+            )
+    }
+}
+
+struct SettingItemView: View {
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .foregroundStyle(
+                Colors
+                    .Neutral
+                    .Label
+                    .quaternary
+                    .swiftUIColor
+            )
+            .font(.label16R)
+            .padding(.vertical, 14.0)
+    }
+}
+
+#Preview {
+    NavigationView(content: {
+        SettingScreenView()
+    })
+}

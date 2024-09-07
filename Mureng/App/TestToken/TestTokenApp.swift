f9a7448da268bb0bb3ae3936f8bc8881d6826a0d
RoadtoS7 <kshvv975@gmail.com>
2024-09-07 19:15:25 +0900

    Build TestToken app target 추가

---
+++ Mureng/App/TestToken/TestTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTokenApp.swift
+//  TestToken
+//
+//  Created by 김수현 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
